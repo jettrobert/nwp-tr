52cfcfa1689e9056fd29e7c8926112597cbf9f8e
Robert Jett <me@robertjett.com>
2023-09-06 15:27:00 +0200

    Changed the blog template

--- pages/[slug].js
+++ pages/[slug].js
@@ -26,10 +26,34 @@ export async function getStaticProps({ params }) {
 
 export default function Post({ post }) {
     return (
-        <div>
-            <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+        <>
+        <meta charSet="UTF-8" />
+        <title>{post.title}</title>
+        <link rel="stylesheet" href="/styles/blog-style.css" />
+        <link rel="stylesheet" href="https://use.typekit.net/zkz4rdl.css" />
+        <div className="logo-container">
+          <a href="homepage.html">
+            <img src="/blog-template/assets/logo-white.png" alt="Home" />
+          </a>
         </div>
+        <div className="blog-post">
+          <h1 className="title">{post.title}</h1>
+          <p className="date">{new Date(post.published_at).toLocaleDateString()}</p>
+          <img className="cover-photo" src={post.feature_image} alt="Cover Photo" />
+          <div className="markdown-content" dangerouslySetInnerHTML={{ __html: post.html }}></div>
+          <div className="related-links">
+            <a href="#" className="link-box">
+              Link 1
+            </a>
+            <a href="#" className="link-box">
+              Link 2
+            </a>
+            <a href="#" className="link-box">
+              Link 3
+            </a>
+          </div>
+          <footer>New World Person 2023</footer>
+        </div>
+      </>
     );
-}
-
+  }
