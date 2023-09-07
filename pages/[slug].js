8d2d34041dc08cb9728cef88c9624120b6bf4e85
Robert Jett <me@robertjett.com>
2023-09-07 15:46:12 +0200

    Created homepage, fixed everything, MVP compelete

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,10 +1,11 @@
 import GhostContentAPI from '@tryghost/content-api';
+import Link from 'next/link';
 
 // Initialize the Ghost API
 const api = new GhostContentAPI({
-  url: process.env.GHOST_API_URL,
-  key: process.env.GHOST_CONTENT_API_KEY,
-  version: 'v3'
+    url: process.env.GHOST_API_URL,
+    key: process.env.GHOST_CONTENT_API_KEY,
+    version: 'v4.0'
 });
 
 export async function getStaticPaths() {
@@ -27,33 +28,34 @@ export async function getStaticProps({ params }) {
 export default function Post({ post }) {
     return (
         <>
-        <meta charSet="UTF-8" />
-        <title>{post.title}</title>
-        <link rel="stylesheet" href="/blog-style.css" />
-        <link rel="stylesheet" href="https://use.typekit.net/zkz4rdl.css" />
-        <div className="logo-container">
-          <a href="homepage.html">
-            <img src="/logo-white.svg" alt="Home" />
-          </a>
-        </div>
-        <div className="blog-post">
-          <h1 className="title">{post.title}</h1>
-          <p className="date">{new Date(post.published_at).toLocaleDateString()}</p>
-          <img className="cover-photo" src={post.feature_image} alt="Cover Photo" />
-          <div className="markdown-content" dangerouslySetInnerHTML={{ __html: post.html }}></div>
-          <div className="related-links">
-            <a href="#" className="link-box">
-              Link 1
-            </a>
-            <a href="#" className="link-box">
-              Link 2
-            </a>
-            <a href="#" className="link-box">
-              Link 3
-            </a>
-          </div>
-          <footer>New World Person 2023</footer>
-        </div>
-      </>
+            <meta charSet="UTF-8" />
+            <title>{post.title}</title>
+            <link rel="stylesheet" href="/blog-style.css" />
+            <link rel="stylesheet" href="https://use.typekit.net/zkz4rdl.css" />
+            <div className="logo-container">
+                <Link href="/">
+                        <img src="/logo-white.svg" alt="Home" />
+                </Link>
+
+            </div>
+            <div className="blog-post">
+                <h1 className="title">{post.title}</h1>
+                <p className="date">{new Date(post.published_at).toLocaleDateString()}</p>
+                <img className="cover-photo" src={post.feature_image} alt="Cover Photo" />
+                <div className="markdown-content" dangerouslySetInnerHTML={{ __html: post.html }}></div>
+                <div className="related-links">
+                    <a href="#" className="link-box">
+                        Link 1
+                    </a>
+                    <a href="#" className="link-box">
+                        Link 2
+                    </a>
+                    <a href="#" className="link-box">
+                        Link 3
+                    </a>
+                </div>
+                <footer>New World Person 2023</footer>
+            </div>
+        </>
     );
-  }
+}
