a5fd2d62d53e937cf9448c9faca13e09346a42f4
Robert Jett <me@robertjett.com>
2023-09-12 12:48:23 +0200

    Added language tag.

--- pages/index.js
+++ pages/index.js
@@ -135,6 +135,7 @@ export async function getStaticProps() {
   try {
     posts = await api.posts.browse({
       limit: 15, // Feel free to adjust the limit
+      filter: 'tags:[tr]' // Only fetch posts with the 'tr' tag
     });
   } catch (error) {
     console.error(error);
